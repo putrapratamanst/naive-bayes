4319382a6b40f2c39b3094090c37c79443c53564
Putra Pratama Nasution <putrapratamanst@gmail.com>
2020-09-03 19:55:24 +0700

    pretty url

--- environments/heroku/common/config/main-local.php
+++ environments/heroku/common/config/main-local.php
@@ -27,5 +27,17 @@ return [
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
     ],
 ];
